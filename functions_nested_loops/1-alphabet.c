304456adf9b58c031c8835509215d0d1b436876a
James <james.hamby@atlasschool.com>
2024-05-21 19:12:55 +0000

    Displays the alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "main.h"
-#include "stdio.h"
 /**
  * print_alphabet - Entry point
- * Return:0
+ * Return: 0
  */
-
 void print_alphabet(void)
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
+	{
 		_putchar(letter);
+	}
 	_putchar('\n');
 }
