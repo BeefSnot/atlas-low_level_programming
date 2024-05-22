7d5c11b5382d7f4f0ab9ded9a52cf93c8ad93c5d
James <james.hamby@atlasschool.com>
2024-05-22 02:59:30 +0000

    Added 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers.
+ * @num1: The first intege.
+ * @num2: The second integer.
+ *
+ * Return: The result.
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
