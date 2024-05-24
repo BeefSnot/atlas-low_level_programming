c701729f4f5c8f853880c4e05e8236bddcb19a57
James <james.hamby@atlasschool.com>
2024-05-24 13:26:15 -0500

    Testing first script

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+
+/**
+ * _isupper - uppercase letters
+ * @c: Check Character
+ *
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+
+}
