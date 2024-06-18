6c90dad6731e6661586fabac7a4a21235e8bbba3
James <jameshamby2@icloud.com>
2024-06-17 20:26:49 -0500

    testing

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Function returns pointer 2 dimensional
+ *
+ *
+ * @width: Width of grid
+ * @height: Height of grid
+ *
+ * Description: Each grid element initialized to 0
+ *
+ * Return: NULL on failure, otherwise return to allocated grid
+ *
+ *
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int
+}
