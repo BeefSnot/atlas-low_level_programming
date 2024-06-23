c6252b7c2ffbbcb8b4c9a708e72f31a214bd32fb
James <jameshamby2@icloud.com>
2024-06-23 18:47:15 -0500

    testing this

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,38 +1,67 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
+
 /**
-  * new_dog - creates a new type of doggo struct
-  * @name: struct parameter name
-  * @age: struct parameter age
-  * @owner: struct parameter owner
-  * Return: returns pointer to buffer of datatype dog
-  */
+ * new_dog - Function that creates a new doggo
+ *
+ * @name: Name of the pupper
+ * @age: Age of the doggo
+ * @owner: Owner of puppo
+ *
+ * Description:
+ *
+ * Return: Pointer to new dog, NULL if fails
+ *
+ */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int nlen, olen, i;
-	dog_t *doggy;
+	dog_t *new_dog;
+	int name_len = 0;
+	int owner_len = 0;
+	int n; /* counter */
 
-	nlen = olen = 0;
-	while (name[nlen++])
+	/* calc length of name string */
+	while (name[name_len++])
 		;
-	while (owner[olen++])
+
+	/* calc length of owner string */
+	while (owner[owner_len++])
 		;
-	doggy = malloc(sizeof(dog_t));
-	if (doggy == NULL)
+
+	/* allocate mem for dog struct */
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
 		return (NULL);
 
-	doggy->name = malloc(nlen * sizeof(doggy->name));
-	if (doggy == NULL)
+	/* allocate mem for name string in new dog struct */
+	new_dog->name = malloc(sizeof(char) * (name_len));
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
 		return (NULL);
-	for (i = 0; i < nlen; i++)
-		doggy->name[i] = name[i];
+	}
 
-	doggy->age = age;
+	/* copy name string to allocated memory */
+	for (n = 0; n < name_len; n++)
+	       new_dog->name[n] = name[n];
 
-	doggy->owner = malloc(olen * sizeof(doggy->owner));
-	if (doggy == NULL)
+	/* allocate mem for owner string in new dog struct */
+	new_dog->owner = malloc(sizeof(char) * (owner_len));
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
 		return (NULL);
-	for (i = 0; i < olen; i++)
-		doggy->owner[i] = owner[i];
-	return (doggy);
+	}
+
+	/* copy owner string to allocated memory */
+	for (n = 0; n < owner_len; n++)
+		new_dog->owner[n] = owner[n];
+
+	/* set age of new dog */
+	new_dog->age = age;
+
+	return (new_dog);
 }
