334a98dbc35036be86d2b30238d320030c430440
James <jameshamby2@icloud.com>
2024-06-23 18:48:50 -0500

    testing

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,67 +1,38 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
-
 /**
- * new_dog - Function that creates a new doggo
- *
- * @name: Name of the pupper
- * @age: Age of the doggo
- * @owner: Owner of puppo
- *
- * Description:
- *
- * Return: Pointer to new dog, NULL if fails
- *
- */
-
+  * new_dog - creates a new struct of type dog
+  * @name: struct parameter name
+  * @age: struct parameter age
+  * @owner: struct parameter owner
+  * Return: returns pointer to buffer of datatype dog
+  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-	int name_len = 0;
-	int owner_len = 0;
-	int n; /* counter */
+	int nlen, olen, i;
+	dog_t *doggy;
 
-	/* calc length of name string */
-	while (name[name_len++])
+	nlen = olen = 0;
+	while (name[nlen++])
 		;
-
-	/* calc length of owner string */
-	while (owner[owner_len++])
+	while (owner[olen++])
 		;
-
-	/* allocate mem for dog struct */
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
+	doggy = malloc(sizeof(dog_t));
+	if (doggy == NULL)
 		return (NULL);
 
-	/* allocate mem for name string in new dog struct */
-	new_dog->name = malloc(sizeof(char) * (name_len));
-	if (new_dog->name == NULL)
-	{
-		free(new_dog);
+	doggy->name = malloc(nlen * sizeof(doggy->name));
+	if (doggy == NULL)
 		return (NULL);
-	}
+	for (i = 0; i < nlen; i++)
+		doggy->name[i] = name[i];
 
-	/* copy name string to allocated memory */
-	for (n = 0; n < name_len; n++)
-	       new_dog->name[n] = name[n];
+	doggy->age = age;
 
-	/* allocate mem for owner string in new dog struct */
-	new_dog->owner = malloc(sizeof(char) * (owner_len));
-	if (new_dog->owner == NULL)
-	{
-		free(new_dog->name);
-		free(new_dog);
+	doggy->owner = malloc(olen * sizeof(doggy->owner));
+	if (doggy == NULL)
 		return (NULL);
-	}
-
-	/* copy owner string to allocated memory */
-	for (n = 0; n < owner_len; n++)
-		new_dog->owner[n] = owner[n];
-
-	/* set age of new dog */
-	new_dog->age = age;
-
-	return (new_dog);
+	for (i = 0; i < olen; i++)
+		doggy->owner[i] = owner[i];
+	return (doggy);
 }
