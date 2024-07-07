af7452a1e72f411a6e96c047259f014687310bad
James <jameshamby2@icloud.com>
2024-07-06 22:26:13 -0500

    Changed the code to my second loop

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,12 +8,16 @@
   */
 size_t print_list(const list_t *h)
 {
-	size_t i;
-
-	for (i = 0; h; i++)
+	size_t life;
+		life = 0;
+	while (h != NULL)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
+		life++;
 	}
-	return (i);
+	return (life);
 }
