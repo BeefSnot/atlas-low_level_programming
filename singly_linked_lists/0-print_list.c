a5d64245433d5f301be1e7cbe1a8029d842dcca6
James <jameshamby2@icloud.com>
2024-07-06 23:12:37 -0500

    Add node to the end

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,6 +1,7 @@
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include "lists.h"
-#include <stdlib.h>
 /**
   * print_list - prints all elements of a list_t list
   * @h: singly linked list to print
---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * add_node_end - adds a new node
+ * to the end of any given list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *k, *p;
+	size_t nchar;
+
+	k = malloc(sizeof(list_t));
+	if (k == NULL)
+		return (NULL);
+
+	k->str = strdup(str);
+
+	for (nchar = 0; str[nchar]; nchar++)
+		;
+
+	k->len = nchar;
+	k->next = NULL;
+	p = *head;
+
+	if (p == NULL)
+	{
+		*head = k;
+	}
+	else
+	{
+		while (p->next != NULL)
+			p = p->next;
+		p->next = k;
+	}
+
+	return (*head);
+}
