b132b023c40025e4af312b2c709877afe94ca255
James <jameshamby2@icloud.com>
2024-07-08 23:52:19 -0500

    Create file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,34 +1,39 @@
+#include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include "main.h"
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
-
 /**
- * create_file - Creates new file and fills it with content.
+ * create_file - creates a file and puts text in it
+ * with 600 perms (do not change if it exists)
  *
- * @filename: File to create.
- * @text_content: text to add to the new file.
+ * @filename: name for file
+ * @text_content: text to put into file
  *
- * Return: Always 1 on success, -1 on Failure
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int err, len, fd;
+	int file;
+	int length = 0, inlen = 0;
+	char *ptr;
 
-	err = len = fd = 0;
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-		if (fd < 0)
-			return (-1);
-	while (text_content && text_content[len])
-		len++;
-
-	err = write(fd, text_content, len);
-	if (err < 0)
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file == -1)
 		return (-1);
-	close(fd);
+
+	if (text_content != NULL)
+	{
+		for (inlen = 0, ptr = text_content; *ptr; ptr++)
+			inlen++;
+		length = write(file, text_content, inlen);
+	}
+
+	if (close(file) == -1 || inlen != length)
+		return (-1);
 	return (1);
 }
